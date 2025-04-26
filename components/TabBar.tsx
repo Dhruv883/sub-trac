6238f5d46c7b77d6c8d1ebcbcb6fd45a493cbb13
Dhruv Dedhia <dhruvdedhia03@gmail.com>
2025-04-26 13:32:25 +0530

    feat: TabBar completed

--- components/TabBar.tsx
+++ components/TabBar.tsx
@@ -7,11 +7,12 @@ import ProfileIcon from "@/assets/icons/profile.svg";
 
 const TabBar = ({ state, descriptors, navigation }: any) => {
   const getIcon = (routeName: string, isFocused: boolean) => {
-    const color = isFocused ? "#FF5722" : "#888888";
+    const color = isFocused ? "#ffe0c2" : "#ffffff";
 
     const iconProps = {
       width: 30,
       height: 30,
+      stroke: color,
     };
 
     switch (routeName) {
@@ -31,7 +32,7 @@ const TabBar = ({ state, descriptors, navigation }: any) => {
   };
 
   return (
-    <View className="flex-row justify-between items-center px-10 py-4 border bg-background-primary-dark">
+    <View className="dark flex-row justify-between items-center px-10 py-4 border-t border-t-[var(--border)] bg-[var(--background)]">
       {state.routes.map((route: any, index: number) => {
         const { options } = descriptors[route.key];
         const isFocused = state.index === index;
@@ -55,7 +56,9 @@ const TabBar = ({ state, descriptors, navigation }: any) => {
             accessibilityLabel={options.tabBarAccessibilityLabel}
             testID={options.tabBarTestID}
             onPress={onPress}
-            className="h-14 w-14 items-center justify-center"
+            className={`h-14 w-14 items-center justify-center rounded-2xl ${
+              isFocused ? "bg-[var(--secondary)]" : null
+            }`}
             key={index}
           >
             {getIcon(route.name, isFocused)}
